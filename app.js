a510d214f3708a8dcd8d9207a37e197e19df3175
abhishektiwari003 <abhishektiwari@gofynd.com>
2026-07-27 15:06:34 +0530

    ch

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Now push water its raining outside 2026 I shown o");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
